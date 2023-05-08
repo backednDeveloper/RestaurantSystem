19afac163252d1250efd70019bfccfb18a48958e
Bank System <emilaze77@gmail.com>
2023-05-08 11:42:37 +0500

    Restaurant Project

--- src/restourantMenus.java
+++ src/restourantMenus.java
@@ -6,26 +6,10 @@ public class restourantMenus {
         );
         switch (typeOfProduct) {
             case 1:
-                boolean found = false;
-                for (meals meals : globalDatas.mealsData) {
-                    meals.getInfoMeals();
-                    found = true;
-                }
-                if (!found) {
-                    System.out.println("Not yet information. ");
-                    return homePanel.panel();
-                }
+
                 break;
             case 2:
-                boolean found1 = false;
-                for (drinks drinks : globalDatas.drinksData) {
-                    drinks.getInfo();
-                    found1 = true;
-                }
-                if (!found1) {
-                    System.out.println("Not yet information. ");
-                    return homePanel.panel();
-                }
+
                 break;
             default:
                 System.out.println("Wrong choose !!! ");
@@ -37,12 +21,21 @@ public class restourantMenus {
         );
         switch (typeOfProduct1) {
             case 1:
+                boolean found = false;
+                for (meals meals : globalDatas.mealsData) {
+                    meals.getInfoMeals();
+                    found = true;
+                }
+                if (!found) {
+                    System.out.println("Not yet information. ");
+                    return homePanel.panel();
+                }
                 int ordernumbers = 1;
                 while (ordernumbers >= 1) {
                     boolean sameproductsearch = false;
-                    for (meals meals : globalDatas.mealsData) {
+                    for (meals information : globalDatas.mealsData) {
                         String mealname = printsMethods.inputRequiredString("Enter the name of the product : ");
-                        meals mealsFound = globalDatas.mapMealsString.put(mealname, meals);
+                        meals mealsFound = globalDatas.mapMealsString.put(mealname, information);
                         if (mealsFound != null) {
                             orderInformationDatas orderInformationDatas = new orderInformationDatas(mealname, ordernumbers);
                             globalDatas.orderInformations.add(orderInformationDatas);
@@ -59,26 +52,35 @@ public class restourantMenus {
                 }
                 break;
             case 2:
-                /*String drinkname = printsMethods.inputRequiredString("Enter the name of the product : ");
-                for (drinks drinks : globalDatas.drinksData) {
-                    boolean sameproduct = false;
-                    drinks drinksfound = globalDatas.mapDrinksString.put(drinkname, drinks);
-                    if (drinksfound != null) {
-                        int ordernumbers = 0;
-                        orderInformationDatas orderInformationDatas = new orderInformationDatas(drinkname, ordernumbers);
-                        while (ordernumbers > 0) {
+                boolean found1 = false;
+                for (meals meals : globalDatas.mealsData) {
+                    meals.getInfoMeals();
+                    found1 = true;
+                }
+                if (!found1) {
+                    System.out.println("Not yet information. ");
+                    return homePanel.panel();
+                }
+                int ordernumbers1 = 1;
+                while (ordernumbers1 >= 1) {
+                    boolean sameproductsearch = false;
+                    for (drinks information1 : globalDatas.drinksData) {
+                        String drinkName = printsMethods.inputRequiredString("Enter the name of the product : ");
+                        drinks drinkFound = globalDatas.mapDrinksString.put(drinkName, information1);
+                        if (drinkFound != null) {
+                            orderInformationDatas orderInformationDatas = new orderInformationDatas(drinkName, ordernumbers1);
                             globalDatas.orderInformations.add(orderInformationDatas);
-                            ordernumbers++;
-                            System.out.println("Your order number " + ordernumbers + " has been confirmed." + "\n" +
-                                    "Order informations : " + globalDatas.orderInformations.get(ordernumbers).getOrderInformations()
-                            );
+                            globalDatas.orderInformations.get(orderInformationDatas.getOrderNumbers()).getOrderInformations();
+                            ordernumbers1++;
+                            break;
                         }
+                        sameproductsearch = true;
                     }
-                    if (!sameproduct) {
+                    if (!sameproductsearch) {
                         System.out.println("Please select an item on the menu. ");
                         return restourantMenus.restourantMenus();
                     }
-                }*/
+                }
                 break;
             default:
                 System.out.println("Wrong added. ");
