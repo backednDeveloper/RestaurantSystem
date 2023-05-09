edbefb0222fd0d4d39f80e6592d280677185ee53
Bank System <emilaze77@gmail.com>
2023-05-09 13:59:07 +0500

    Restaurant Project

--- src/orderInformationDatas.java
+++ src/orderInformationDatas.java
@@ -1,10 +1,15 @@
 public class orderInformationDatas {
-    String order;
-    int orderNumbers;
+    private String order;
+    private int orderNumbers;
+    private int price;
+    private int orderAdress;
+    private String getOrderInformations;
 
-    public orderInformationDatas(String order, int orderNumbers) {
+    public orderInformationDatas(String order, int orderNumbers , int price , int orderAdress) {
         this.order = order;
         this.orderNumbers = orderNumbers;
+        this.price=price;
+        this.orderAdress=orderAdress;
     }
 
     public String getOrder() {
@@ -14,10 +19,36 @@ public class orderInformationDatas {
     public int getOrderNumbers() {
         return orderNumbers;
     }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public void setOrderNumbers(int orderNumbers) {
+        this.orderNumbers = orderNumbers;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getTable() {
+        return orderAdress;
+    }
+
+    public void setTable(int orderAdress) {
+        this.orderAdress = orderAdress;
+    }
     public String getOrderInformations (){
-        System.out.println("---------------- Order ---------------- "  + "\n" + "Product : " + order + "\n" +
-                "Order number : " + orderNumbers + "\n"
-                );
-        return  getOrder();
+        System.out.println("Product : " + order + "\n" +
+                "Oreder product numbers : " + orderNumbers + "\n" +
+                "Oreder table numbers : " + orderAdress + "\n" +
+                "Oreder total price : " + price + "\n"
+        );
+        return null;
     }
 }
--- src/restourantMenus.java
+++ src/restourantMenus.java
@@ -1,37 +1,53 @@
 public class restourantMenus {
     public static String restourantMenus() {
-        while (true) {
-            int typeOfProduct1 = printsMethods.inputRequiredInt("1 - For the food products " + "\n" +
-                    "2 - For the  drinks products " + "\n" +
-                    "Select one of the options above : "
-            );
-            switch (typeOfProduct1) {
-                case 1:
-                    System.out.println("Menu : ");
-                    int index = 1;
-                    for (meals information : globalDatas.mealsData) {
-                        System.out.println(index + information.getInfoMeals());
-                        index++;
-                    }
-                    int choose = printsMethods.inputRequiredInt("Choose product (0 is exit): ");
-                    if (choose == 0) {
+        int typeOfProduct1 = printsMethods.inputRequiredInt("1 - For the food products " + "\n" +
+                "2 - For the  drinks products " + "\n" +
+                "Select one of the options above : "
+        );
+        switch (typeOfProduct1) {
+            case 1:
+                System.out.println("-------- Menu -------- ");
+                int index = 1;
+                for (meals information : globalDatas.mealsData) {
+                    System.out.println(index + "-" + information.getInfoMeals());
+                    index++;
+                    int choose = printsMethods.inputRequiredInt("Enter the number of products you choose from the menu : ");
+                    int choose1 = printsMethods.inputRequiredInt("Choose product from menu (0 is exit): ");
+                    if (choose1 == 0) {
                         break;
-                    } else if (choose < 1 || choose > globalDatas.mealsData.size()) {
+                    } else if (choose1 < 1 || choose1 > globalDatas.mealsData.size()) {
                         System.out.println("Wrong choose!");
                     } else {
-                        meals choosProduct = globalDatas.mealsData.get(choose - 1);
-                        int choosNumber = printsMethods.inputRequiredInt("Choose product order numbers : ");
-                        orderInformationDatas informationDatas = new orderInformationDatas(choosProduct.getMeal(), choosNumber);
-                        globalDatas.orderInformations.add(informationDatas);
-                        System.out.println(informationDatas.getOrderInformations());
+                        meals choosProduct = globalDatas.mealsData.get(choose1 - 1);
+                        int choosNumber = printsMethods.inputRequiredInt("Enter how many of this item you want : ");
+                        int chooseAdress = printsMethods.inputRequiredInt("Write your adress : ");
+                        int price = information.getMealprice() * choosNumber;
+                        boolean look = false;
+                        if (information.getMealprice() == price / choosNumber) {
+                            orderInformationDatas informationDatas = new orderInformationDatas(choosProduct.getMeal(), choosNumber, price, chooseAdress);
+                            globalDatas.orderInformations.add(informationDatas);
+                            System.out.println(informationDatas.getOrderInformations());
+                            look = true;
+                        }
+                        if (!look) {
+                            orderInformationDatas informationDatas1 = new orderInformationDatas(choosProduct.getMeal(), choosNumber, price, chooseAdress);
+                            informationDatas1.setPrice(price + (informationDatas1.getPrice()));
+                            globalDatas.orderInformations.add(informationDatas1);
+                            System.out.println("The number of the product you choose from the menu : " + information.getPlaceInMenu()
+                                    + "\n" + "Your #" + choose + " order"
+                                    + "\n" + "Your order informations " + "\n" + informationDatas1.getOrderInformations()
+                                    + "\n" + "Your order total price is : " + "\n" + price
+                            );
+                        }
                     }
-                    break;
-                case 2:
-                    break;
-                default:
-                    System.out.println("Wrong added. ");
-            }
-            return restourantMenus.restourantMenus();
+                }
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("Wrong added. ");
         }
+        return restourantMenus.restourantMenus();
+
     }
 }
